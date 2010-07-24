3328959f7e0e6423acb17e190c15a7474250c296
Josh Price <joshcp@gmail.com>
2010-07-24 17:16:50 +1000

    Specs to cover outer loop and vertices

--- stl_spec.rb
+++ stl_spec.rb
@@ -46,7 +46,42 @@ describe "STL treetop parser" do
       facet.facet_start.ni.text_value.should == "-1.0"
       facet.facet_start.nj.text_value.should == "0.0"
       facet.facet_start.nk.text_value.should == "1.0"
+      facet.outer_loop.text_value.should == ""
+    end
+  end
+
+  describe "named solid with one facet and an outer loop" do
+    it "should parse outer loop with no vertices" do
+      result = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\nendloop\nendfacet\nendsolid onefacet")
+      result.should_not be_nil
+      facet = result.facet
+      facet.text_value.should_not be_nil
+      facet.facet_start.ni.text_value.should == "-1.0"
+      facet.facet_start.nj.text_value.should == "0.0"
+      facet.facet_start.nk.text_value.should == "1.0"
+      facet.outer_loop.text_value.should_not == ""
+      facet.outer_loop.text_value.should == "  outer loop\nendloop\n"
     end
 
+    it "should parse single vertex in outer loop" do
+      result = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\n    vertex -1.0 0.0 1.0\nendloop\nendfacet\nendsolid onefacet")
+      result.should_not be_nil
+      result.facet.outer_loop.vertex.text_value.should == "    vertex -1.0 0.0 1.0\n"
+      result.facet.outer_loop.vertex.elements[0].vx.text_value.should == "-1.0"
+      result.facet.outer_loop.vertex.elements[0].vy.text_value.should == "0.0"
+      result.facet.outer_loop.vertex.elements[0].vz.text_value.should == "1.0"
+    end
+
+    it "should parse multiple vertices in outer loop" do
+      result = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\n    vertex -1.0 0.0 1.0\n    vertex -2.0 0.0 2.0\nendloop\nendfacet\nendsolid onefacet")
+      result.should_not be_nil
+      result.facet.outer_loop.vertex.text_value.should == "    vertex -1.0 0.0 1.0\n    vertex -2.0 0.0 2.0\n"
+      result.facet.outer_loop.vertex.elements[0].vx.text_value.should == "-1.0"
+      result.facet.outer_loop.vertex.elements[0].vy.text_value.should == "0.0"
+      result.facet.outer_loop.vertex.elements[0].vz.text_value.should == "1.0"
+      result.facet.outer_loop.vertex.elements[1].vx.text_value.should == "-2.0"
+      result.facet.outer_loop.vertex.elements[1].vy.text_value.should == "0.0"
+      result.facet.outer_loop.vertex.elements[1].vz.text_value.should == "2.0"
+    end
   end
 end
