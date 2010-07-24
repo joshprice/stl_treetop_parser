0e39046950902275a01773b2b286cea0458c61a4
Josh Price <joshcp@gmail.com>
2010-07-24 19:06:13 +1000

    Numbers now have a value() method

--- stl_spec.rb
+++ stl_spec.rb
@@ -41,9 +41,9 @@ describe "STL" do
       result.should_not be_nil
       facet = result.facet.elements[0]
       facet.text_value.should_not be_nil
-      facet.facet_start.ni.text_value.should == "-1.0"
-      facet.facet_start.nj.text_value.should == "0.0"
-      facet.facet_start.nk.text_value.should == "1.0"
+      facet.facet_start.ni.value.should == -1.0
+      facet.facet_start.nj.value.should == 0.0
+      facet.facet_start.nk.value.should == 1.0
       facet.outer_loop.text_value.should == ""
     end
   end
@@ -54,9 +54,9 @@ describe "STL" do
       result.should_not be_nil
       facet = result.facet.elements[0]
       facet.text_value.should_not be_nil
-      facet.facet_start.ni.text_value.should == "-1.0"
-      facet.facet_start.nj.text_value.should == "0.0"
-      facet.facet_start.nk.text_value.should == "1.0"
+      facet.facet_start.ni.value.should == -1.0
+      facet.facet_start.nj.value.should == 0.0
+      facet.facet_start.nk.value.should == 1.0
       facet.outer_loop.text_value.should_not == ""
       facet.outer_loop.text_value.should == "outer loop\nendloop\n"
     end
@@ -66,9 +66,9 @@ describe "STL" do
       result.should_not be_nil
       facet = result.facet.elements[0]
       facet.outer_loop.vertex.text_value.should == "vertex -1.0 0.0 1.0\n"
-      facet.outer_loop.vertex.elements[0].vx.text_value.should == "-1.0"
-      facet.outer_loop.vertex.elements[0].vy.text_value.should == "0.0"
-      facet.outer_loop.vertex.elements[0].vz.text_value.should == "1.0"
+      facet.outer_loop.vertex.elements[0].vx.value.should == -1.0
+      facet.outer_loop.vertex.elements[0].vy.value.should == 0.0
+      facet.outer_loop.vertex.elements[0].vz.value.should == 1.0
     end
 
     it "should parse multiple vertices in outer loop" do
@@ -76,12 +76,12 @@ describe "STL" do
       result.should_not be_nil
       facet = result.facet.elements[0]
       facet.outer_loop.vertex.text_value.should == "vertex -1.0 0.0 1.0\n    vertex -2.0 0.0 2.0\n"
-      facet.outer_loop.vertex.elements[0].vx.text_value.should == "-1.0"
-      facet.outer_loop.vertex.elements[0].vy.text_value.should == "0.0"
-      facet.outer_loop.vertex.elements[0].vz.text_value.should == "1.0"
-      facet.outer_loop.vertex.elements[1].vx.text_value.should == "-2.0"
-      facet.outer_loop.vertex.elements[1].vy.text_value.should == "0.0"
-      facet.outer_loop.vertex.elements[1].vz.text_value.should == "2.0"
+      facet.outer_loop.vertex.elements[0].vx.value.should == -1.0
+      facet.outer_loop.vertex.elements[0].vy.value.should == 0.0
+      facet.outer_loop.vertex.elements[0].vz.value.should == 1.0
+      facet.outer_loop.vertex.elements[1].vx.value.should == -2.0
+      facet.outer_loop.vertex.elements[1].vy.value.should == 0.0
+      facet.outer_loop.vertex.elements[1].vz.value.should == 2.0
     end
   end
   
@@ -112,28 +112,28 @@ describe "STL" do
       facets[0].facet_start.ni.text_value.should == "0.0"
       facets[0].facet_start.nj.text_value.should == "0.0"
       facets[0].facet_start.nk.text_value.should == "-1.0"
-      facets[0].outer_loop.vertex.elements[0].vx.text_value.should == "116.377952755906"
-      facets[0].outer_loop.vertex.elements[0].vy.text_value.should == "66.1023622047244"
-      facets[0].outer_loop.vertex.elements[0].vz.text_value.should == "0.0"
-      facets[0].outer_loop.vertex.elements[1].vx.text_value.should == "0.0"
-      facets[0].outer_loop.vertex.elements[1].vy.text_value.should == "0.0"
-      facets[0].outer_loop.vertex.elements[1].vz.text_value.should == "0.0"
-      facets[0].outer_loop.vertex.elements[2].vx.text_value.should == "0.0"
-      facets[0].outer_loop.vertex.elements[2].vy.text_value.should == "66.1023622047244"
-      facets[0].outer_loop.vertex.elements[2].vz.text_value.should == "0.0"
+      facets[0].outer_loop.vertex.elements[0].vx.value.should == 116.377952755906
+      facets[0].outer_loop.vertex.elements[0].vy.value.should == 66.1023622047244
+      facets[0].outer_loop.vertex.elements[0].vz.value.should == 0.0
+      facets[0].outer_loop.vertex.elements[1].vx.value.should == 0.0
+      facets[0].outer_loop.vertex.elements[1].vy.value.should == 0.0
+      facets[0].outer_loop.vertex.elements[1].vz.value.should == 0.0
+      facets[0].outer_loop.vertex.elements[2].vx.value.should == 0.0
+      facets[0].outer_loop.vertex.elements[2].vy.value.should == 66.1023622047244
+      facets[0].outer_loop.vertex.elements[2].vz.value.should == 0.0
       facets[1].text_value.should_not be_nil
       facets[1].facet_start.ni.text_value.should == "0.0"
       facets[1].facet_start.nj.text_value.should == "0.0"
       facets[1].facet_start.nk.text_value.should == "-1.0"
-      facets[1].outer_loop.vertex.elements[0].vx.text_value.should == "0.0"
-      facets[1].outer_loop.vertex.elements[0].vy.text_value.should == "0.0"
-      facets[1].outer_loop.vertex.elements[0].vz.text_value.should == "0.0"
-      facets[1].outer_loop.vertex.elements[1].vx.text_value.should == "116.377952755906"
-      facets[1].outer_loop.vertex.elements[1].vy.text_value.should == "116.377952755906"
-      facets[1].outer_loop.vertex.elements[1].vz.text_value.should == "0.0"
-      facets[1].outer_loop.vertex.elements[2].vx.text_value.should == "116.377952755906"
-      facets[1].outer_loop.vertex.elements[2].vy.text_value.should == "0.0"
-      facets[1].outer_loop.vertex.elements[2].vz.text_value.should == "0.0"
+      facets[1].outer_loop.vertex.elements[0].vx.value.should == 0.0
+      facets[1].outer_loop.vertex.elements[0].vy.value.should == 0.0
+      facets[1].outer_loop.vertex.elements[0].vz.value.should == 0.0
+      facets[1].outer_loop.vertex.elements[1].vx.value.should == 116.377952755906
+      facets[1].outer_loop.vertex.elements[1].vy.value.should == 116.377952755906
+      facets[1].outer_loop.vertex.elements[1].vz.value.should == 0.0
+      facets[1].outer_loop.vertex.elements[2].vx.value.should == 116.377952755906
+      facets[1].outer_loop.vertex.elements[2].vy.value.should == 0.0
+      facets[1].outer_loop.vertex.elements[2].vz.value.should == 0.0
     end
   end
 end
