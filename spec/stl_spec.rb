ed7453906124aff87c6c48cb79ba74e32d6f88b1
Josh Price <joshcp@gmail.com>
2010-07-26 13:06:32 +1000

    Rename result to solid in specs for the start of a more OO approach

--- spec/stl_spec.rb
+++ spec/stl_spec.rb
@@ -8,39 +8,39 @@ describe "STL" do
   
   describe "solid with no facets" do
     it "should parse an anonymous solid" do
-      result = @p.parse("solid \nendsolid \n")
-      result.should_not be_nil
-      result.solid_start.text_value.should == "solid "
-      result.solid_start.solid_name.text_value.should == ""
-      result.solid_end.text_value.should == "endsolid "
+      solid = @p.parse("solid \nendsolid \n")
+      solid.should_not be_nil
+      solid.solid_start.text_value.should == "solid "
+      solid.solid_start.solid_name.text_value.should == ""
+      solid.solid_end.text_value.should == "endsolid "
     end
 
     it "should handle EOF on last line" do
-      result = @p.parse("solid \nendsolid ")
-      result.should_not be_nil
-      result.solid_start.solid_name.text_value.should == ""
+      solid = @p.parse("solid \nendsolid ")
+      solid.should_not be_nil
+      solid.solid_start.solid_name.text_value.should == ""
     end
 
     it "should parse a named solid" do
-      result = @p.parse("solid name\nendsolid name\n")
-      result.should_not be_nil
-      result.solid_start.solid_name.text_value.should == "name"
-      result.solid_end.solid_name.text_value.should == "name"
+      solid = @p.parse("solid name\nendsolid name\n")
+      solid.should_not be_nil
+      solid.solid_start.solid_name.text_value.should == "name"
+      solid.solid_end.solid_name.text_value.should == "name"
     end
 
     it "should have no facets" do
-      result = @p.parse("solid name\nendsolid name\n")
-      result.should_not be_nil
-      result.facet.text_value.should == ""
-      result.facet.elements.should == []
+      solid = @p.parse("solid name\nendsolid name\n")
+      solid.should_not be_nil
+      solid.facet.text_value.should == ""
+      solid.facet.elements.should == []
     end
   end
 
   describe "named solid with one facet but no outer loop" do
     it "should parse facet" do
-      result = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\nendfacet\nendsolid onefacet")
-      result.should_not be_nil
-      facet = result.facet.elements[0]
+      solid = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\nendfacet\nendsolid onefacet")
+      solid.should_not be_nil
+      facet = solid.facet.elements[0]
       facet.text_value.should_not be_nil
       facet.facet_start.ni.value.should == -1.0
       facet.facet_start.nj.value.should == 0.0
@@ -51,9 +51,9 @@ describe "STL" do
 
   describe "named solid with one facet and an outer loop" do
     it "should parse outer loop with no vertices" do
-      result = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\nendloop\nendfacet\nendsolid onefacet")
-      result.should_not be_nil
-      facet = result.facet.elements[0]
+      solid = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\nendloop\nendfacet\nendsolid onefacet")
+      solid.should_not be_nil
+      facet = solid.facet.elements[0]
       facet.text_value.should_not be_nil
       facet.facet_start.ni.value.should == -1.0
       facet.facet_start.nj.value.should == 0.0
@@ -64,9 +64,9 @@ describe "STL" do
     end
 
     it "should parse single vertex in outer loop" do
-      result = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\n    vertex -1.0 0.0 1.0\nendloop\nendfacet\nendsolid onefacet")
-      result.should_not be_nil
-      facet = result.facet.elements[0]
+      solid = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\n    vertex -1.0 0.0 1.0\nendloop\nendfacet\nendsolid onefacet")
+      solid.should_not be_nil
+      facet = solid.facet.elements[0]
       facet.outer_loop.vertex.text_value.should == "vertex -1.0 0.0 1.0\n"
       facet.outer_loop.vertex.elements[0].vx.value.should == -1.0
       facet.outer_loop.vertex.elements[0].vy.value.should == 0.0
@@ -74,9 +74,9 @@ describe "STL" do
     end
 
     it "should parse multiple vertices in outer loop" do
-      result = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\n    vertex -1.0 0.0 1.0\n    vertex -2.0 0.0 2.0\nendloop\nendfacet\nendsolid onefacet")
-      result.should_not be_nil
-      facet = result.facet.elements[0]
+      solid = @p.parse("solid onefacet\nfacet normal -1.0 0.0 1.0\n  outer loop\n    vertex -1.0 0.0 1.0\n    vertex -2.0 0.0 2.0\nendloop\nendfacet\nendsolid onefacet")
+      solid.should_not be_nil
+      facet = solid.facet.elements[0]
       facet.outer_loop.vertex.text_value.should == "vertex -1.0 0.0 1.0\n    vertex -2.0 0.0 2.0\n"
       facet.outer_loop.vertex.elements[0].vx.value.should == -1.0
       facet.outer_loop.vertex.elements[0].vy.value.should == 0.0
@@ -107,9 +107,9 @@ describe "STL" do
         endfacet
         endsolid rect
       STL
-      result = @p.parse(stl_doc)
-      result.should_not be_nil
-      facets = result.facet.elements
+      solid = @p.parse(stl_doc)
+      solid.should_not be_nil
+      facets = solid.facet.elements
       facets[0].text_value.should_not be_nil
       facets[0].facet_start.ni.value.should == 0.0
       facets[0].facet_start.nj.value.should == 0.0
